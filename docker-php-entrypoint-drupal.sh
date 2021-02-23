4412178fc749397c6eb77e551497610b4432a56c
Michael Büchner <m.buechner@dnb.de>
2021-02-23 21:48:58 +0100

    Using supercronic as cron servce and supervisor to start services

--- docker-php-entrypoint-drupal.sh
+++ docker-php-entrypoint-drupal.sh
@@ -6,11 +6,6 @@ echo "Start Drupal Cache Rebuild..."
 /var/www/html/vendor/bin/drush --root /var/www/html/web cache-rebuild
 
 set -e
-echo "Export Environment variables for Cron job..."
-printenv | sed 's/^\(.*\)$/export \1/g' | sed 's/=/="/' | sed 's/$/"/' > /tmp/ddbgoenv.sh
-chmod 700 /tmp/ddbgoenv.sh
-echo "Start Cron service..."
-service cron start
 
 docker-php-entrypoint
 
