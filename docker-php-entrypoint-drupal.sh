2a73d41eec030f8edcfb4f52a9639a53e378930b
Michael Büchner <m.buechner@dnb.de>
2021-03-12 14:42:42 +0100

    Added option MAINTENANCE_ON_STARTUP (if set to 'no' there will be no Drupal Update DB and Cache Rebuild)

--- docker-php-entrypoint-drupal.sh
+++ docker-php-entrypoint-drupal.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
-set +e
-echo "Start Drupal Update DB..."
-/var/www/html/vendor/bin/drush --root /var/www/html/web updatedb
-echo "Start Drupal Cache Rebuild..."
-/var/www/html/vendor/bin/drush --root /var/www/html/web cache-rebuild
+k#!/bin/sh
+set +e;
 
-set -e
+if [ ${MAINTENANCE_ON_STARTUP+x} = "no" ]; then
+        echo "NOT doing Drupal Update DB and Cache Rebuild...";
+else
+        echo "Start Drupal Update DB...";
+        /var/www/html/vendor/bin/drush --root /var/www/html/web updatedb;
+        echo "Start Drupal Cache Rebuild...";
+        /var/www/html/vendor/bin/drush --root /var/www/html/web cache-rebuild;
+fi;
 
-docker-php-entrypoint
+set -e;
 
-exec "$@"
+docker-php-entrypoint;
+
+exec "$@";
+
