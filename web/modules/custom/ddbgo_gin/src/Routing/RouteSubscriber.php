be67b4e5e97bdad7ac2efeb81c3c7be27d837b42
Michael Büchner <mbuechner@users.noreply.github.com>
2026-08-31 16:56:19 +0200

    Fixed DDBGO-44

--- web/modules/custom/ddbgo_gin/src/Routing/RouteSubscriber.php
+++ web/modules/custom/ddbgo_gin/src/Routing/RouteSubscriber.php
@@ -3,6 +3,8 @@
 namespace Drupal\ddbgo_gin\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\node\NodeTypeInterface;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
@@ -17,6 +19,23 @@ final class RouteSubscriber extends RouteSubscriberBase {
     if ($route = $collection->get('key_auth.user_key_auth_form')) {
       $route->setRequirement('_ddbgo_administer_user_api_keys_access', 'TRUE');
     }
+
+    if ($route = $collection->get('node.add')) {
+      $route->setDefault('_title_callback', self::class . '::nodeAddTitle');
+    }
+  }
+
+  /**
+   * Returns consistent add-form titles for DDBgo content types.
+   */
+  public static function nodeAddTitle(NodeTypeInterface $node_type): string|TranslatableMarkup {
+    if (in_array($node_type->id(), ['kwe', 'aggregator', 'person', 'bestand'], TRUE)) {
+      return $node_type->label() . ' hinzufügen';
+    }
+
+    return new TranslatableMarkup('Create @name', [
+      '@name' => $node_type->label(),
+    ]);
   }
 
 }
